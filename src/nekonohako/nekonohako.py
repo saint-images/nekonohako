7a5e5bf84f0aae47e98344dfb46991c8ec9263ba
saint images <41950379+saint-images@users.noreply.github.com>
2022-06-06 21:54:29 +0200

    I can't believe it isn't JS!

--- src/nekonohako/nekonohako.py
+++ src/nekonohako/nekonohako.py
@@ -3,7 +3,7 @@ class nekonohako:
     REQUEST_URL = 'https://catbox.moe/user/api.php'
 
     def __init__(self, config):
-        self.user_hash = config.user_hash
+        self.user_hash = config['user_hash']
 
     def test(self):
         print(self.user_hash)
