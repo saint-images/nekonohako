7eef94a787e6c8b4b15093ddbe519e650c23b1fe
saint images <41950379+saint-images@users.noreply.github.com>
2022-06-07 22:14:08 +0200

    add file uploading

--- src/nekonohako/core/catbox.py
+++ src/nekonohako/core/catbox.py
@@ -1,3 +1,5 @@
+import ntpath
+import mimetypes
 import requests
 from .const import RequestParams, Methods
 
@@ -8,21 +10,46 @@ class Catbox:
     def __init__(self, config):
         self.user_hash = config['user_hash']
 
+    def __prepare_params(self, params):
+        if self.user_hash != '':
+            params[RequestParams.USER_HASH] = self.user_hash
+
+        return params
+
     @staticmethod
-    def __request(params):
-        result = requests.post(Catbox.REQUEST_URL, data=params)
-        print(result.text)
+    def __read_file(file_path):
+        try:
+            with open(file_path, 'rb') as file:
+                file_content = file.read()
+                file_name = ntpath.basename(file_path)
+                file_type = mimetypes.guess_type(file_path)[0]
+                return (file_name, file_content, file_type)
+        except IOError:
+            return None
 
     def upload_url(self, url):
         params = {
             RequestParams.REQUEST_TYPE: Methods.URL_UPLOAD,
             RequestParams.URL: url,
         }
+        params = self.__prepare_params(params)
 
-        if self.user_hash != '':
-            params[RequestParams.USER_HASH] = self.user_hash
-
-        Catbox.__request(params)
+        result = requests.post(Catbox.REQUEST_URL, data=params)
+        return result.text
 
     def upload_file(self, file_path):
-        pass
+        file_info = Catbox.__read_file(file_path)
+        if file_info is None:
+            return None
+
+        params = {
+            RequestParams.REQUEST_TYPE: Methods.FILE_UPLOAD,
+        }
+        params = self.__prepare_params(params)
+
+        files = {
+            RequestParams.FILE_TO_UPLOAD: file_info
+        }
+
+        result = requests.post(Catbox.REQUEST_URL, data=params, files=files)
+        return result.text
